57c78a4cca0ff9ce110d300859310e5658f418af
jessdudar <jessicadudar@trentu.ca>
2021-02-02 15:52:13 -0500

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Jessica Dudar on 2021-02-02.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
